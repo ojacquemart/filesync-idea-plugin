c139b08cfd88d5f8f5e9333af580c8f3655911f6
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-06-11 22:31:50 +0200

    Add some log and test if fileEvent is from refresh action.

--- src/main/java/org/filesync/idea/plugin/files/FilesCollector.java
+++ src/main/java/org/filesync/idea/plugin/files/FilesCollector.java
@@ -1,6 +1,7 @@
 package org.filesync.idea.plugin.files;
 
 import com.intellij.history.LocalHistory;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
@@ -11,19 +12,24 @@ import java.util.List;
 
 public class FilesCollector implements BulkFileListener {
 
-    public void before(@NotNull List<? extends VFileEvent> vFileEvents) {
-        for (VFileEvent event : vFileEvents) {
-            final Object requestor = event.getRequestor();
-            if (requestor instanceof FileDocumentManager ||
-                    requestor instanceof PsiManager ||
-                    requestor == LocalHistory.VFS_EVENT_REQUESTOR) {
-                FilesSynchronyzer.sync();
-            }
-        }
+    private static final Logger LOGGER = Logger.getInstance(FilesCollector.class);
 
+    public void before(@NotNull List<? extends VFileEvent> vFileEvents) {
     }
 
     public void after(@NotNull List<? extends VFileEvent> vFileEvents) {
+        for (VFileEvent event : vFileEvents) {
+            final Object requestor = event.getRequestor();
+
+            if (requestor instanceof FileDocumentManager ||
+                    requestor instanceof PsiManager ||
+                    requestor == LocalHistory.VFS_EVENT_REQUESTOR
+                    || event.isFromRefresh()) {
+                LOGGER.debug("Launch sync...");
+                FilesSynchronyzer.sync();
+                break;
+            }
+        }
     }
 
 }
